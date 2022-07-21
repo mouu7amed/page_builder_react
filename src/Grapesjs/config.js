76b39e5a2a24af68376e602d79ef34cc74144268
mouu7amed <mouu7amed@gmail.com>
2022-07-21 19:06:23 +0200

    adjust editor config file

---
+++ src/Grapesjs/config.js
@@ -0,0 +1,59 @@
+import grapesjs from "grapesjs";
+import gjsPresetWebage from "grapesjs-preset-webpage";
+import { API_HOST } from "../api";
+import { uploadAsset } from "../redux/features/assets/assetSlice";
+
+const geditorConfig = (assets, pageId, uploadImage, currentUser, dispatch) => {
+  const editor = grapesjs.init({
+    container: "#editor",
+    storageManager: {
+      type: "remote",
+      autosave: true,
+      stepsBeforeSave: 1,
+      contentTypeJson: true,
+      storeComponents: true,
+      storeStyles: true,
+      storeHtml: true,
+      storeCss: true,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      options: {
+        remote: {
+          urlStore: `${API_HOST}pages/${pageId}/content`,
+          urlLoad: `${API_HOST}pages/${pageId}/content`,
+        },
+      },
+    },
+    assetManager: {
+      storageType: "",
+      storeOnChange: true,
+      storeAfterUpload: true,
+      assets: [],
+      autoAdd: true,
+      upload: false,
+      uploadFile: async function (e) {
+        var files = e.dataTransfer ? e.dataTransfer.files : e.target.files;
+        await uploadImage(files[0]).then((url) => {
+          const assetInfo = {
+            type: files[0].type,
+            src: url,
+            userId: currentUser._id,
+          };
+          dispatch(uploadAsset(assetInfo));
+        });
+      },
+    },
+    plugins: [gjsPresetWebage],
+    pluginsOpts: {
+      gjsPresetWebage: {},
+    },
+  });
+
+  const srcs = assets.map((asset) => asset.src);
+  editor.AssetManager.add(srcs);
+
+  return editor;
+};
+
+export default geditorConfig;
--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -1,15 +1,10 @@
 import { Box } from "@mui/material";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { API_HOST } from "../../api";
 import { useAuth } from "../../context/AuthProvider";
 import { useSelector, useDispatch } from "react-redux";
-import grapesjs from "grapesjs";
-import gjsPresetWebage from "grapesjs-preset-webpage";
-import {
-  fetchAssets,
-  uploadAsset,
-} from "../../redux/features/assets/assetSlice";
+import { fetchAssets } from "../../redux/features/assets/assetSlice";
+import geditorConfig from "../../Grapesjs/config";
 
 const Editor = () => {
   const { pageId } = useParams();
@@ -23,54 +18,7 @@ const Editor = () => {
   }, [dispatch]);
 
   useEffect(() => {
-    const editor = grapesjs.init({
-      container: "#editor",
-      storageManager: {
-        type: "remote",
-        autosave: true,
-        stepsBeforeSave: 1,
-        contentTypeJson: true,
-        storeComponents: true,
-        storeStyles: true,
-        storeHtml: true,
-        storeCss: true,
-        headers: {
-          "Content-Type": "application/json",
-        },
-        options: {
-          remote: {
-            urlStore: `${API_HOST}pages/${pageId}/content`,
-            urlLoad: `${API_HOST}pages/${pageId}/content`,
-          },
-        },
-      },
-      assetManager: {
-        storageType: "",
-        storeOnChange: true,
-        storeAfterUpload: true,
-        assets: [],
-        autoAdd: true,
-        upload: false,
-        uploadFile: async function (e) {
-          var files = e.dataTransfer ? e.dataTransfer.files : e.target.files;
-          await uploadImage(files[0]).then((url) => {
-            const assetInfo = {
-              type: files[0].type,
-              src: url,
-              userId: currentUser._id,
-            };
-            dispatch(uploadAsset(assetInfo));
-          });
-        },
-      },
-      plugins: [gjsPresetWebage],
-      pluginsOpts: {
-        gjsPresetWebage: {},
-      },
-    });
-
-    const srcs = assets.map((asset) => asset.src);
-    editor.AssetManager.add(srcs);
+    geditorConfig(assets, pageId, uploadImage, currentUser, dispatch);
   }, [assets, pageId, uploadImage, currentUser, dispatch]);
 
   return (
